20b1a80d51f5e0f6d7a87c9108ba85e1a8e0faae
Kelsey <kelseykchan@gmail.com>
2015-01-19 16:46:03 -0500

    degrees to radians

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -216,32 +216,32 @@ a desired xyz coordinate position.
 */
 // FYI, Arduino uses RADIANS, not degrees.
 void calculateDegrees (int x, int y, int z) {
-  const float pi = 3.14159265259;
-//  int x = 2; // x we give
-//  int y = 2; // y we give
-//  int z = 2; // z we give
-  float phi = 2; // wrist angle we give, between end effector and line of forearm
-  float length1 = 1; // height from base to shoulder
-  float length2 = 1; // length from shoulder to elbow
-  float length3 = 1; // length from elbow to wrist
-  float ro = 1; //length from wrist to end effector
-  float r = 0; // distance to end effector from base on base plane
-  float theta1 = 0;  // base angle
-  float theta2 = 0; // angle of the servo at the shoulder, from horizontal
-  float theta3 = 0; // angle of the servo at the elbow, between wrist and shoulder
-  r = sqrt(pow(x, 2.0) +pow(y, 2.0)); // pythagoras
-  theta1 = atan2(y,x); // trig
-  float length5 = sqrt(pow(length3,2.0) + pow(ro,2.0) - 2*length3*ro*cos(180-phi)); // law of cosines to find the length from the wrist to the end effector
-  float phi2 = acos((pow(-ro,2.0) - pow(length5,2.0) + pow(length3, 2.0))/(-2*length5*length3)); //angle from wrist-elbow-end effector
-  float length4 = sqrt(pow(r,2.0) + pow(z-length1,2.0)); // length from shoulder to end effector
-  theta3 = acos((pow(length4,2.0) - pow(length5,2.0) - pow(length2,2.0))/(-2*length5*length2)) + phi2; // elbow angle using law of cosines, correcting for the fact that the end effector placement angle is not the same as the elbow angle due to phiand ro being nonzero.
-  float theta4 = atan2(z-length1, r); // angle from horizontal to end effector
-  float theta5 = acos((pow(length5,2.0) - pow(length2,2.0) - pow(length4,2.0))/(-2*length2*length4)); // angle from theta4 to humerus
-  theta2 =theta4 +theta5; // adding to get theta 2
-  // I am Africa
-  desiredDegrees[0] = theta1; // desired base angle
-  desiredDegrees[1] = theta2; // desired shoulder angle
-  desiredDegrees[2] = theta3; // desired elbow angle
+    const float pi = 3.14159265259;
+  //  int x = 2; // x we give
+  //  int y = 2; // y we give
+  //  int z = 2; // z we give
+    float phi = 2; // wrist angle we give, between end effector and line of forearm
+    float length1 = 1; // height from base to shoulder
+    float length2 = 1; // length from shoulder to elbow
+    float length3 = 1; // length from elbow to wrist
+    float ro = 1; //length from wrist to end effector
+    float r = 0; // distance to end effector from base on base plane
+    float theta1 = 0;  // base angle
+    float theta2 = 0; // angle of the servo at the shoulder, from horizontal
+    float theta3 = 0; // angle of the servo at the elbow, between wrist and shoulder
+    r = sqrt(pow(x, 2.0) +pow(y, 2.0)); // pythagoras
+    theta1 = atan2(y,x); // trig
+    float length5 = sqrt(pow(length3,2.0) + pow(ro,2.0) - 2*length3*ro*cos(pi-phi)); // law of cosines to find the length from the wrist to the end effector
+    float phi2 = acos((pow(-ro,2.0) - pow(length5,2.0) + pow(length3, 2.0))/(-2*length5*length3)); //angle from wrist-elbow-end effector
+    float length4 = sqrt(pow(r,2.0) + pow(z-length1,2.0)); // length from shoulder to end effector
+    theta3 = acos((pow(length4,2.0) - pow(length5,2.0) - pow(length2,2.0))/(-2*length5*length2)) + phi2; // elbow angle using law of cosines, correcting for the fact that the end effector placement angle is not the same as the elbow angle due to phiand ro being nonzero.
+    float theta4 = atan2(z-length1, r); // angle from horizontal to end effector
+    float theta5 = acos((pow(length5,2.0) - pow(length2,2.0) - pow(length4,2.0))/(-2*length2*length4)); // angle from theta4 to humerus
+    theta2 =theta4 +theta5; // adding to get theta 2
+    // I am Africa
+    desiredDegrees[0] = theta1; // desired base angle
+    desiredDegrees[1] = theta2; // desired shoulder angle
+    desiredDegrees[2] = theta3; // desired elbow angle
 }
 
 /*
@@ -252,7 +252,7 @@ int degreesToPulse(int angle_Degree, int pulseMin, int pulseMax){
   if(angle_Degree > 180){
       pulse_length = map(angle_Degree, 0, 360, pulseMin, pulseMax);
    }
-  else if (angle_Degree < 180){
+  else if (angle_Degree <= 180){
       pulse_length = map(angle_Degree, 0, 180, pulseMin, pulseMax);
     }
   return pulse_length;
